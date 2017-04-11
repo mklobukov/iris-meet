7fa19f2e472c603b1eede29aef474ce2bd8d009a
mklobukov <mk3396@drexel.edu>
2017-04-11 09:10:04 -0400

    removed reference to main-footer (removed the HOC) because id=footer can be added directly to horizontal-wrapper

--- src/backups/horizontal-wrapper.jsx
+++ src/backups/horizontal-wrapper.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import MainFooter from './main-footer';
 
-export default function HorizontalWrapper(props) {
+function HorizontalWrapper(props) {
   return (
-      <div id = "footer">
-        <div className={props.isHidden ? "horizontal-wrapper videoBarHide" : "horizontal-wrapper videoBarShow"}>
-            {props.children}
-        </div>
+      <div className={props.isHidden ? "horizontal-wrapper videoBarHide" : "horizontal-wrapper videoBarShow"}>
+          {props.children}
       </div>
   );
 }
 
-//export default MainFooter(HorizontalWrapper)
-
+export default MainFooter(HorizontalWrapper)
 
 /*
 import React from 'react';
