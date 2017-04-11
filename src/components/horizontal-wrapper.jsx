cf4fea0ed5f6a0de16d961c9ddb453a0f6cf82b3
mklobukov <mk3396@drexel.edu>
2017-04-11 09:13:45 -0400

    cleaned up horizontal-wrapper

--- src/components/horizontal-wrapper.jsx
+++ src/components/horizontal-wrapper.jsx
@@ -10,27 +10,3 @@ export default function HorizontalWrapper(props) {
       </div>
   );
 }
-
-//export default MainFooter(HorizontalWrapper)
-
-
-/*
-import React from 'react';
-import MainFooter from './main-footer';
-
-class HorizontalWrapper extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className={this.props.isHidden ? "horizontal-wrapper videoBarHide" : "horizontal-wrapper videoBarShow"}>
-                {this.props.children}
-            </div>
-        );
-    }
-}
-
-export default MainFooter(HorizontalWrapper)
-*/
