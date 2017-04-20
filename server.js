23d023873e6160b0d4efc9148b244b61d43e01ef
Klobukov <mklobu200@hqsml-152198.cable.comcast.com>
2017-04-20 12:39:09 -0400

    changed server.js to get key and cert from paths specified in config.json

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+import Config from ('./config')
 
 var app = express()
 
 // serve our static stuff like index.css
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
   // and drop 'public' in the middle of here
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
-app.listen(PORT, function() {
+/*app.listen(PORT, function() {
   console.log('Production Express server running at localhost:' + PORT)
-})
+})*/
+var options = Config.options
+
+app.server = https.createServer(options, app);
+
+app.server.listen('443', '0.0.0.0', () => {
+console.log(`Server running on ${app.server.address().address}${app.server.address().port}`);
+});
