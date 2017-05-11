3e2b0715419aabf050ef6bc935c00859e9ad7335
mklobukov <mark_klobukov@drexel.edu>
2017-05-11 13:28:13 -0400

    fixed the issue with dom speaker for >2 participants, disabled switching for 2 participants

--- src/components/main.js
+++ src/components/main.js
@@ -189,36 +189,39 @@ componentWillReceiveProps = (nextProps) => {
   _onDominantSpeakerChanged(dominantSpeakerEndpoint) {
     //let participant = track.getParticipantId();
     //let baseId = participant.replace(/(-.*$)|(@.*$)/,'');
-      const matchedConnection = this.props.remoteVideos.find((connection) => {
-        let participantId = connection.participantJid;
-        participantId = participantId.substring(participantId.indexOf("/")+1, participantId.indexOf("@iris-meet.comcast.com"))
-        //participantId = participantId.substring(0, participantId.indexOf("/"))
-        dominantSpeakerEndpoint = dominantSpeakerEndpoint.substring(0, dominantSpeakerEndpoint.lastIndexOf("@"))
-        const endPoint = participantId.substring(participantId.lastIndexOf("/")+1)
-        console.log("endpoint and dom: " + endPoint + ", " + dominantSpeakerEndpoint)
-        return endPoint === dominantSpeakerEndpoint;
-    });
+    console.log("Got a new dominant speaker notification\nLooking through remotes...")
+    const matchedConnection = this.props.remoteVideos.find((connection) => {
+      let participantId = connection.participantJid;
+      participantId = participantId.substring(participantId.indexOf("/")+1, participantId.indexOf("@iris-meet.comcast.com"))
+      let dom = dominantSpeakerEndpoint.substring(0, dominantSpeakerEndpoint.lastIndexOf("@"))
+      const endPoint = participantId.substring(participantId.lastIndexOf("/")+1)
+      console.log("endpoint and dom: " + endPoint + ", " + dom)
+      return endPoint === dom;
+      });
 
-    console.log('FOUND DOMINANT SPEAKER: ');
-    console.log(matchedConnection);
+    // if (!matchedConnection) {
+    //   //just for testing
+    //     console.log("No matched connection among remotes. Trying local...")
+    //     console.log(this.props.localVideos)
+    //     console.log("Compare with: ", this.props.remoteVideos)
+    //   }
+
     if (matchedConnection) {
+      console.log("New dominant speaker among remotes: ", matchedConnection.participantJid)
+      console.log("Compare to domEndpoint: ", dominantSpeakerEndpoint)
+      //entering this if statement implies that the dominant speaker is remote
+      //no further checks are necessary
       this.props.changeDominantSpeaker(matchedConnection.id)
-      //change main view only if current speaker is remote
-      if (matchedConnection.id !== this.props.localVideos[0].id) {
-        this.props.VideoControl('remote', matchedConnection.id, this.props.localVideos, this.props.remoteVideos)
-      }
+      this.props.VideoControl('remote', matchedConnection.id, this.props.localVideos, this.props.remoteVideos)
 
     } else if (this.props.localVideos.length > 0) {
+      console.log("Local speaker is dominant: ", this.props.localVideos[0])
+      console.log("compare to endpoint: ", dominantSpeakerEndpoint)
+      console.log(dominantSpeakerEndpoint)
       // no remote participants found so assume it is local speaker
-      //Leave this outside the if-statement to initialize a call and have
-      //the only participant's video on the main screen
-
-      //change dominant speaker but do not put local video on the main screen
-      //if there are any remote videos
+      //change dominant speaker but don't change main view, keep displaying
+      //the most recent remote dominant speaker
       this.props.changeDominantSpeaker(this.props.localVideos[0].id)
-      if (this.props.remoteVideos.length === 0) {
-        this.props.VideoControl('local', this.props.localVideos[0].id, this.props.localVideos, this.props.remoteVideos)
-      }
     }
   }
 
@@ -421,10 +424,7 @@ _screenShareControl() {
 
       <HorizontalWrapper isHidden={this.state.isVideoBarHidden}>
           {this.props.localVideos.map((connection) => {
-            console.log('Main.js, rendering local horiz box:');
-            console.log(connection);
-
-            return (!this._isDominant(connection.id) && this.props.remoteVideos.length > 0) ? (
+            return (this.props.remoteVideos.length > 0 ) ? (
               <HorizontalBox
                 key={connection.id}
                 type='local'
@@ -446,7 +446,9 @@ _screenShareControl() {
           })}
           {this.props.remoteVideos.map((connection) => {
             if (connection) {
-              return !this._isDominant(connection.id) ? (
+              const displayHorizontalBox = !this._isDominant(connection.id) && this.props.remoteVideos.length > 1 ;
+              console.log("Display HB for ", connection.id, "? -- ", displayHorizontalBox)
+              return displayHorizontalBox ? (
                 <HorizontalBox
                   key={connection.id}
                   type='remote'
